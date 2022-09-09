157058350221edb0b44e8409e926b5587b213e45
gloria oduor <gloriaoduor@gmail.com>
2022-09-09 21:00:15 +0300

    whitespaces

--- supermarket_reviewer/src/NavBar.js
+++ supermarket_reviewer/src/NavBar.js
@@ -2,7 +2,6 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import './App.css';
 
-
 function NavBar() {
   return <div className="navBar">
     <NavLink to="/" className="navLink">Home</NavLink> 
